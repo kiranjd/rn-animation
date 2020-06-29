f5afa3827b4cd2f670cea92002a9e4a5e5e4affb
kiranjd <kiranjd8@gmail.com>
2020-06-29 09:00:01 +0530

    Adds Storybook

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,2 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { ButtonOnShow } from './animations';
-import RatingStars from './animations/RatingStars';
+export default from '../storybook';
 
-export default () => (
-  <View
-    style={{
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#000',
-    }}
-  >
-    {/* <ButtonOnShow /> */}
-    <RatingStars />
-    <Text>Whre are the stars?</Text>
-  </View>
-);
