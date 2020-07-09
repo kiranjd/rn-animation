f40b9e287d0b8014ff0608e564e4931ece3d0581
kiranjd <kiranjd8@gmail.com>
2020-07-09 07:44:00 +0530

    Adds chat; converts remaining js into ts

--- storybook/stories/index.tsx
+++ storybook/stories/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Text } from 'react-native';
 
 import { storiesOf } from '@storybook/react-native';
-
 import { CenterView } from './decorators';
+
 import RatingStars from '../../src/animations/RatingStars';
-import { Text } from 'react-native';
+import Chat from '../../src/animations/Chat';
 
 storiesOf('Animations', module)
   .addDecorator((getStory: Function) => <CenterView>{getStory()}</CenterView>)
-  .add('animated rating stars', () => <RatingStars />)
+  .add('chat', () => <Chat />)
+  .add('rating stars', () => <RatingStars />)
   .add('Hello World!', () => <Text>Hello World!</Text>);
